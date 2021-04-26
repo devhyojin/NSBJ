aebf2c40ba8b6794cc98b51f4c904cc71a651b0b
Jamie Lee <itzjamie96@gmail.com>
2021-04-26 15:46:23 +0900

    [Feat] add backend initial project
    - backend project setup
    - com.daynight.birdmouse

---
+++ exec/backend/birdmouse/src/main/java/com/daynight/birdmouse/BirdmouseApplication.java
@@ -0,0 +1,13 @@
+package com.daynight.birdmouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdmouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdmouseApplication.class, args);
+	}
+
+}
---
+++ exec/backend/birdmouse/src/test/java/com/daynight/birdmouse/BirdmouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daynight.birdmouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdmouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
