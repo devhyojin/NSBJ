12e0bf3730eacc79a9c1e1558283f79093ea1448
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-05-02 21:51:57 +0900

    [Feat: FE] Edit Confirm Modal Design
    - Confirm Modal의 글자크기와 버튼 간격을 수정하고, scss파일명을
      변경했습니다.

--- exec/frontend/src/components/ConfirmModal.tsx
+++ exec/frontend/src/components/ConfirmModal.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import '../styles/_modal.scss';
+import '../styles/_confirmModal.scss';
 
 export default function CharacterModal({ changeConfirmStatus }) {
   return (
     <div className="modal-mask">
-      <div className="modal-wrapper">
-        <div className="modal-container">
-          <div className="modal-header">
-            <p>
-              닉네임은 하루에 <strong>한 번만</strong> 변경 가능합니다.
-            </p>
-            <p>정말 변경하시겠습니까?</p>
-          </div>
-          <div className="modal-body">
-            <button className="confirmBtn btnRed" type="submit">
-              YES
-            </button>
-            <button onClick={changeConfirmStatus} className="confirmBtn btnBlue" type="submit">
-              NO
-            </button>
-          </div>
+      <div className="modal-container">
+        <div className="modal-header">
+          <p>
+            닉네임은 하루에 <strong>한 번만</strong> 변경 가능합니다.
+          </p>
+          <p>정말 변경하시겠습니까?</p>
+        </div>
+        <div className="modal-body">
+          <button className="confirmBtn btnRed" type="submit">
+            YES
+          </button>
+          <button onClick={changeConfirmStatus} className="confirmBtn btnBlue" type="submit">
+            NO
+          </button>
         </div>
       </div>
     </div>
