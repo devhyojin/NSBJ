ba10d7c28b58141dbc928b44ab3c8da2c1295e18
이동희 <ldh29768@gmail.com>
2021-05-03 16:24:43 +0900

    savePoint

--- exec/front-end-pwa/src/components/KakaoLogin.tsx
+++ exec/front-end-pwa/src/components/KakaoLogin.tsx
@@ -1,27 +1,55 @@
+// import axios from 'axios'
+import '../styles/_landing.scss'
+import kakaoBtn from '../assets/kakao/kakao_login_medium_narrow.png'
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
+interface response {
+  accessToken: string,
+  expiresIn: number,
+  refreshToken: string,
+  refreshTokenExpiresIn: number,
+  scope: string,
+  tokenType: string,
+}
 
 
-// const JAVASCRIPT_KEY = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY
-export default function KakaoLogin() {
-
-  const { Kakao } = window
-
-  const loginWithKakao = (): any => {
-    Kakao.Auth.authorize({
-      redirectUri: 'http://localhost:3000'
+const { Kakao } = window;
+console.log(window, Kakao)
+export default function KakaoLogin({ login }: any) {
+  const kakaoLoginClickHandler = () => {
+    Kakao.Auth.login({
+      success: (response: response) => {
+        login(response)
+      },
+      fail: (err: any) => alert(err)
     })
   }
+
+  // const logout = () => {
+  //   // Kakao.Auth.logout(function () {
+  //   //   console.log(Kakao.Auth.getAccessToken())
+  //   // })
+  //   Kakao.API.request({
+  //     url: '/v1/user/unlink',
+  //     success: (response: any) => {
+  //       console.log(response);
+  //     },
+  //     fail: (error: any) => {
+  //       console.log(error);
+  //     },
+  //   });
+  // }
 
 
 
   return (
-    <>
-      <div id='custom-login-btn' onClick={loginWithKakao} role='button' tabIndex={0} onKeyDown={() => null}>
-        <img
-          src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
-          width="152"
-          alt='test'
-        />
-      </div>
-    </>
+    <button onClick={kakaoLoginClickHandler} type="button" className='kakao__login__btn'>
+      <img src={kakaoBtn} alt="kakaoBtn" width="100%" />
+    </button>
   )
 }
