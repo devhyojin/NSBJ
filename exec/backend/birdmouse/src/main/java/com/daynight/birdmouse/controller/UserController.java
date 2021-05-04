ca63dd80bca5fbd526e042c0c6a05e4ccb8d5fdd
kyungseo <kyungseo55@gmail.com>
2021-05-04 12:18:51 +0900

    [Chore:BE] Update ApiParam & RequestParam - controller에서 ApiParam과 RequestParam값을 변경하여 swagger 에러 해결

--- exec/backend/birdmouse/src/main/java/com/daynight/birdmouse/controller/UserController.java
+++ exec/backend/birdmouse/src/main/java/com/daynight/birdmouse/controller/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
 
     @GetMapping(value = "")
     @ApiOperation(value = "사용자의 마이페이지 정보 불러오기")
-    public Object getMypage(@ApiParam(value = "user id", required = true)
+    public Object getMypage(@ApiParam(value = "user_id", required = true, example = "12345")
                             @RequestParam Long id) {
         Response result = userService.getMypage(id);
         return new ResponseEntity<>(result, HttpStatus.OK);
@@ -31,31 +31,31 @@ public class UserController {
 
     @PatchMapping(value = "/badge")
     @ApiOperation(value = "사용자의 칭호를 변경하기")
-    public Object changeBadge(@ApiParam(value = "user_id", required = true)
-                             @RequestParam Long id, int badge) {
+    public Object changeBadge(@RequestParam(value = "user_id", required = true) Long id,
+                              @RequestParam(value = "badge", required = true) int badge) {
         Response result = userService.changeBadge(id, badge);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PatchMapping(value = "/img")
     @ApiOperation(value = "사용자의 프로필 캐릭터를 변경하기")
-    public Object changeProfileImg(@ApiParam(value = "user_id", required = true)
-                             @RequestParam Long id, int profile_img) {
+    public Object changeProfileImg(@RequestParam(value = "user_id", required = true) Long id,
+                                   @RequestParam(value = "badge", required = true) int profile_img) {
         Response result = userService.changeProfileImg(id, profile_img);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PatchMapping(value = "/nickname")
     @ApiOperation(value = "사용자의 닉네임을 변경하기")
-    public Object changeNickname(@ApiParam(value = "user_id", required = true)
-                                 @RequestParam Long id, String mode) {
+    public Object changeNickname(@RequestParam(value = "user_id", required = true) Long id,
+                                 @RequestParam(value = "badge", required = true) String mode){
         Response result = userService.modifiedNickname(id, mode);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PatchMapping(value = "/withdraw")
     @ApiOperation(value = "사용자 탈퇴하기")
-    public Object changeNickname(@ApiParam(value = "user_id", required = true)
+    public Object changeNickname(@ApiParam(value = "user_id", required = true, example = "12345")
                                  @RequestParam Long id) {
         Response result = userService.withdrawUser(id);
         return new ResponseEntity<>(result, HttpStatus.OK);
