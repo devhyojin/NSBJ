9b290dc25196c2e7787c7d99ece86873363cab1c
kyungseo <kyungseo55@gmail.com>
2021-05-04 11:55:02 +0900

    [Chore:BE] Correct typos - requestParam의 Integer를 int로 변경

--- exec/backend/birdmouse/src/main/java/com/daynight/birdmouse/controller/UserController.java
+++ exec/backend/birdmouse/src/main/java/com/daynight/birdmouse/controller/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
     @PatchMapping(value = "/badge")
     @ApiOperation(value = "사용자의 칭호를 변경하기")
     public Object changeBadge(@ApiParam(value = "user_id", required = true)
-                             @RequestParam Long id, Integer badge) {
+                             @RequestParam Long id, int badge) {
         Response result = userService.changeBadge(id, badge);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
@@ -40,7 +40,7 @@ public class UserController {
     @PatchMapping(value = "/img")
     @ApiOperation(value = "사용자의 프로필 캐릭터를 변경하기")
     public Object changeProfileImg(@ApiParam(value = "user_id", required = true)
-                             @RequestParam Long id, Integer profile_img) {
+                             @RequestParam Long id, int profile_img) {
         Response result = userService.changeProfileImg(id, profile_img);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
