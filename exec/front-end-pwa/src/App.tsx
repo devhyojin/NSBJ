4a1b07afe94bcbfa163d25788cd4564500aacf96
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-05-11 14:47:04 +0900

    [Feat: FE] Add PageNotFound
    - PageNotFound 페이지를 추가했습니다.

--- exec/front-end-pwa/src/App.tsx
+++ exec/front-end-pwa/src/App.tsx
@@ -5,6 +5,7 @@ import LandingPage from './routes/LandingPage';
 import MainPage from './routes/MainPage';
 import MyPage from './routes/MyPage';
 import Chat from './routes/Chat';
+import PageNotFound from './routes/PageNotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <PrivateRoute path="/main" component={MainPage} />
       <PrivateRoute path="/mypage" component={MyPage} />
       <PrivateRoute path="/chat" component={Chat} />
+      <PublicRoute component={PageNotFound} />
     </Switch>
   );
 }
