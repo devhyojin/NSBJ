6bf7da8d55abac8a43d4aa9bad3a6477216444ac
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-05-13 00:48:33 +0900

    [Feat: FE] Add screen guide
    - # S04P31A406-165
    - 모바일 화면이 아닐 시, 화면 크기를 모바일에 맞게 조정해달라는 가이드
      메시지 화면이 보이게 설정했습니다.
    - closes S04P31A406-165

--- exec/front-end-pwa/src/App.tsx
+++ exec/front-end-pwa/src/App.tsx
@@ -1,21 +1,31 @@
-import { Switch } from 'react-router-dom';
-import PublicRoute from './lib/PublicRoute';
-import PrivateRoute from './lib/PrivateRoute';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import LandingPage from './routes/LandingPage';
 import MainPage from './routes/MainPage';
 import MyPage from './routes/MyPage';
 import Chat from './routes/Chat';
-import PageNotFound from './routes/PageNotFound';
+import birdBasic from './assets/characters/bird/bird_basic.gif';
+import mouseBasic from './assets/characters/mouse/mouse_basic.gif';
+import './styles/_screenGuide.scss';
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute exact path="/" restricted component={LandingPage} />
-      <PrivateRoute path="/main" component={MainPage} />
-      <PrivateRoute path="/mypage" component={MyPage} />
-      <PrivateRoute path="/chat" component={Chat} />
-      <PublicRoute component={PageNotFound} />
-    </Switch>
+    <div className="app">
+      <div className="screenGuide">
+        <div className="characters">
+          <img src={birdBasic} alt="노말짹" />
+          <img src={mouseBasic} alt="노말찍" />
+        </div>
+        {/* <span role="img" aria-label="wink">
+          😉
+        </span> */}
+        <p>본 서비스는 모바일 환경에 최적화되어있습니다!</p>
+      </div>
+      <Route path="/" exact component={LandingPage} />
+      <Route path="/main" component={MainPage} />
+      <Route path="/mypage" component={MyPage} />
+      <Route path="/chat/:regionId" component={Chat} />
+    </div>
   );
 }
 
