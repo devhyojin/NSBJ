e16262140e38b17802bb55f6e5ad4715c8754abe
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-05-13 15:27:54 +0900

    [Feat: FE] Update feedback logic
    - #S04P31A406-159
    - 피드백을 axios로 받아오는 로직을 수정했습니다.
    - 받아온 피드백이 0 초과일 때, 피드백 기능을 사용하지 못하도록
      설정했습니다.

--- exec/front-end-pwa/src/components/Chat/ChatContent.tsx
+++ exec/front-end-pwa/src/components/Chat/ChatContent.tsx
@@ -1,14 +1,14 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import Message from './Message';
 import EnterChat from './EnterChat';
 
-
 interface ChatContentProps {
   data: any;
   mode: string;
   user_id: number;
+  region_id: number;
 }
 
 interface msgProps {
@@ -21,44 +21,54 @@ interface msgProps {
   mouse_name: string;
   badge: any;
   profileImg: number;
-  mode: string
+  mode: string;
 }
 
-export default function ChatContent({ data, mode, user_id }: ChatContentProps): any {
-  const chatContent = useRef() as React.MutableRefObject<HTMLInputElement>
-  let cnt = -1
+export default function ChatContent({ data, mode, user_id, region_id }: ChatContentProps): any {
+  const chatContent = useRef() as React.MutableRefObject<HTMLInputElement>;
+  let cnt = -1;
 
   useEffect(() => {
-    const target = chatContent.current
+    const target = chatContent.current;
     if (!target) return;
-    target.scrollTop = target.scrollHeight
-  })
-
+    target.scrollTop = target.scrollHeight;
+  });
 
   if (!data) {
-    return <div>{null}</div>
+    return <div>{null}</div>;
   }
   return (
-    <div className='chat__content' ref={chatContent}>
+    <div className="chat__content" ref={chatContent}>
       {data.map((msg: msgProps) => {
-        cnt += 1
+        cnt += 1;
         if (msg.type === 'ENTER') {
-          const { message } = msg
-          return <EnterChat key={uuidv4()} enterMessage={message} />
+          const { message } = msg;
+          return <EnterChat key={uuidv4()} enterMessage={message} />;
         }
         // else if (msg.type === 'ANNOUNCE') {
         //   return <Message key={uuidv4()} msg={msg} userId={userId} />
-        // }      
+        // }
 
-        let skipProfile = false
-        console.log(msg.sent_at)
-        if (cnt && data[cnt - 1].sender_id === msg.sender_id && data[cnt - 1].sent_at === msg.sent_at) {
-          skipProfile = true
+        let skipProfile = false;
+        console.log(msg.sent_at);
+        if (
+          cnt &&
+          data[cnt - 1].sender_id === msg.sender_id &&
+          data[cnt - 1].sent_at === msg.sent_at
+        ) {
+          skipProfile = true;
         }
-        return <Message key={uuidv4()} msg={msg} user_id={user_id} mode={mode} skipProfile={skipProfile} />
-
-      })
-      }
+        return (
+          <Message
+            key={uuidv4()}
+            msg={msg}
+            user_id={user_id}
+            region_id={region_id}
+            mode={mode}
+            skipProfile={skipProfile}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
--- exec/front-end-pwa/src/components/Chat/Message.tsx
+++ exec/front-end-pwa/src/components/Chat/Message.tsx
@@ -1,48 +1,23 @@
 import React, { useRef, useState, useEffect } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import ChatButton from './ChatButton';
 
 import '../../styles/_message.scss';
 
-// const SERVER_URL = process.env.REACT_APP_URL;
+const SERVER_URL = process.env.REACT_APP_URL;
 
-export default function Message({ msg, user_id, mode, skipProfile }: any) {
-  const initFeedbackInfo = { id: 0, user_id: 0, receiver_id: 0, receiver_nickname: null };
-  const [feedbackInfo, setFeedbackInfo] = useState(initFeedbackInfo);
+export default function Message({ msg, user_id, region_id, mode, skipProfile }: any) {
   const [feedbackModalStatus, setFeedbackModalStatus] = useState<boolean>(false);
-
-
-  // const getFeedbackInfo = (): void => {
-  //   // 1. 피드백 관련 모든 정보 업데이트 해주기
-  //   axios.get(`${SERVER_URL}/mypage`, { params: { id: userId } }).then((res) => {
-  //     const tempFeedbackInfo = { ...feedbackInfo };
-  //     tempFeedbackInfo[id] = res.feedback;
-  //     tempFeedbackInfo[user_id] = res.user_id;
-  //     tempFeedbackInfo[recevier_id] = res.recevier_id;
-  //     tempFeedbackInfo[recevier_nickname] = res.recevier_nickname;
-  //     setFeedbackInfo(tempFeedbackInfo);
-  //   });
-  // };
   const changeFeedbackModalStatus = (): void => {
-    // 접근 권한이 있을 때만 모달을 열 수 있다. 이미 피드백한 경우, 과거 피드백 당시의 타유저의 id와 닉네임이 현재와 모두 동일하다면, 접근권한이 없어 alert가 뜬다.
-    // if (feedbackInfo.id) {
-    //   setFeedbackModalStatus(!feedbackModalStatus);
-    // } else {
-    //      alert("이미 포인트를 주었습니다.")
-    // }
     setFeedbackModalStatus(!feedbackModalStatus);
   };
-
-
-
-
 
   let coverClassName = 'message__content__cover ';
   let profileClassName = 'message__profile ';
   let messageClassName = 'message ';
-  let badgeClassName = 'nick__badge '
-  let timeClassName = 'time__stamp '
-  const messageCoverName = `message__cover profile__${mode}`
+  let badgeClassName = 'nick__badge ';
+  let timeClassName = 'time__stamp ';
+  const messageCoverName = `message__cover profile__${mode}`;
   const messageRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 
   if (msg.sender_id === 1) {
@@ -50,14 +25,14 @@ export default function Message({ msg, user_id, mode, skipProfile }: any) {
     profileClassName = '';
     coverClassName += 'my__cover ';
   } else if (!skipProfile) {
-    profileClassName += `img__${msg.profile_img}`
+    profileClassName += `img__${msg.profile_img}`;
     messageClassName += 'other__message ';
   } else {
-    badgeClassName += 'display__none'
-    timeClassName += 'display__none'
+    badgeClassName += 'display__none';
+    timeClassName += 'display__none';
   }
 
-  messageClassName += `message__${mode} `
+  messageClassName += `message__${mode} `;
 
   return (
     <div>
@@ -71,7 +46,7 @@ export default function Message({ msg, user_id, mode, skipProfile }: any) {
         >
           <span className={timeClassName}>{msg.sent_at}</span>
         </div>
-        {/* {feedbackModalStatus && <ChatButton mode={msg.mode} feedbackInfo={feedbackInfo} />} */}
+        {feedbackModalStatus && <ChatButton msg={msg} user_id={user_id} region_id={region_id} />}
         <div className={coverClassName}>
           <div className={badgeClassName}>
             {null} <br /> {mode === 'light' ? msg.bird_name : msg.mouse_name}
