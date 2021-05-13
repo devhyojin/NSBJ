635972835d3a2b9e2a0fa2238be9b7a690dee626
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-05-13 10:04:38 +0900

    [Fix: FE] Reset router authority
    - 라우터 접근 제한을 걸었던 설정이 적용되지 않은 상태로 돌아가서
      재적용시켰습니다.

--- exec/front-end-pwa/src/App.tsx
+++ exec/front-end-pwa/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import PublicRoute from './lib/PublicRoute';
+import PrivateRoute from './lib/PrivateRoute';
 import LandingPage from './routes/LandingPage';
 import MainPage from './routes/MainPage';
 import MyPage from './routes/MyPage';
 import Chat from './routes/Chat';
+import PageNotFound from './routes/PageNotFound';
 import birdBasic from './assets/characters/bird/bird_basic.gif';
 import mouseBasic from './assets/characters/mouse/mouse_basic.gif';
 import './styles/_screenGuide.scss';
@@ -16,15 +18,14 @@ function App() {
           <img src={birdBasic} alt="노말짹" />
           <img src={mouseBasic} alt="노말찍" />
         </div>
-        {/* <span role="img" aria-label="wink">
-          😉
-        </span> */}
         <p>본 서비스는 모바일 환경에 최적화되어있습니다!</p>
       </div>
-      <Route path="/" exact component={LandingPage} />
-      <Route path="/main" component={MainPage} />
-      <Route path="/mypage" component={MyPage} />
-      <Route path="/chat/:regionId" component={Chat} />
+      <Switch>
+        <PublicRoute exact path="/" restricted component={LandingPage} />
+        <PrivateRoute path="/main" component={MainPage} />
+        <PrivateRoute path="/mypage" component={MyPage} />
+        <PrivateRoute path="/chat" component={Chat} />
+      </Switch>
     </div>
   );
 }
